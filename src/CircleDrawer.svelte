b317a3d0926374de4a4b87878c463b47c3a88ea0
masond <drmason13@gmail.com>
2020-11-04 17:14:54 +0000

    feat: undo/redo

--- src/CircleDrawer.svelte
+++ src/CircleDrawer.svelte
@@ -2,14 +2,13 @@
     import {v4 as uuid} from 'uuid';
     import { tick } from 'svelte';
 
-    import { circles, history, undo, redo, selected } from './stores.js';
+    import { circles, history, selected, newCircleAction, changedRadiusAction } from './stores.js';
     import Circle from './Circle.svelte';
-    
+
     // parameters
     const defaultRadius = 20; // new circles are created with this radius
 
     // element references
-    let svg; // the svg "canvas" where circles are drawn
     let radiusInput; // the popup dialog's input for altering the selected circle's radius
 
     // state //
@@ -22,15 +21,30 @@
 
     // stores the current radius selected using the popup dialog (modal)
     let currentRadius;
+    // stores the previous radius before confirming the popup dialog (modal) for undo to use to undo the radius change
+    let prevRadius;
 
     // derived values //
 
     // update the radius of the currently selected circle
     $: if ($selected) {
         circles.update(circles => {
+            if (!prevRadius) {
+                prevRadius = circles[$selected].radius;
+            }
             circles[$selected].radius = currentRadius;
             return circles;
         });
+    }
+
+    async function confirmRadiusChoice() {
+        if (currentRadius !== prevRadius) {
+            history.do(changedRadiusAction({ id: $selected, prevRadius, newRadius: currentRadius }));
+        }
+        prevRadius = null;
+
+        await tick();
+        selected.set(null);
     }
 
     // handlers //
@@ -38,12 +52,12 @@
     // selects a circle, moves modal to it and focuses its input
     async function selectCircle(id, e) {
         selected.set(id);
-        const rect = svg.getBoundingClientRect();
         modalPosition = {
-            x: rect.x + e.offsetX,
-            y: rect.y + e.offsetY,
+            x: e.x,
+            y: e.y,
         };
         currentRadius = $circles[id].radius;
+        prevRadius = currentRadius;
         
         await tick();
         radiusInput.focus();
@@ -52,28 +66,31 @@
     // creates a new circle with unique id for referencing
     function newCircle(x, y) {
         const id = uuid();
-        circles.set({
-            ...$circles,
-            [id]: { id, x, y, radius: defaultRadius }
+        circles.update(c => {
+            c[id] = { id, x, y, radius: defaultRadius };
+            return c
         });
+        prevRadius = defaultRadius;
+        // record the new circle in history
+        history.do(newCircleAction({ id, x, y, radius: defaultRadius }));
     }
 </script>
 
 <article class="flex flex-col flex-grow-0 items-center justify-center mb-4 bg-beige-50 px-2 py-4">
     <section id="actions" class="flex flex-row flex-grow-0 items-center justify-center mb-4 bg-beige-50">
-        <button id="undo" on:click={undo} class="btn btn-outline-beige flex-grow-0 mx-2">Undo</button>
-        <button id="redo" on:click={redo} class="btn btn-outline-beige flex-grow-0 mx-2">Redo</button>
+        <button id="undo" on:click={history.undo} class="btn btn-outline-beige flex-grow-0 mx-2">Undo</button>
+        <button id="redo" on:click={history.redo} class="btn btn-outline-beige flex-grow-0 mx-2">Redo</button>
     </section>
     <section id="canvas" class="border">
-        <svg bind:this={svg} on:click={e => newCircle(e.offsetX, e.offsetY)} height="500px" width="500px">
-            {#each Object.values($circles) as {id, x, y, radius}}
+        <svg on:click={e => newCircle(e.offsetX, e.offsetY)} height="500px" width="500px">
+            {#each Object.values($circles) as { id, x, y, radius }}
                 <Circle {id} {x} {y} {radius} {selectCircle}></Circle>
             {/each}
         </svg>
         
-        <article id="modal" class="bg-beige-100 max-w-xs" class:hidden="{$selected === null}" class:absolute="{$selected !== null}" style="left: {modalPosition.x}px; top: {modalPosition.y}px;">
+        <article id="modal" class="bg-beige-100 max-w-xs px-2 rounded border shadow-lg" class:hidden="{$selected === null}" class:absolute="{$selected !== null}" style="left: {modalPosition.x}px; top: {modalPosition.y}px;">
             <p>radius: {currentRadius}</p>
-            <input bind:this={radiusInput} type="range" min="1" max="200" bind:value={currentRadius} on:blur={() => selected.set(null)} />
+            <input bind:this={radiusInput} type="range" min="1" max="200" bind:value={currentRadius} on:blur={() => confirmRadiusChoice()} />
         </article>
     </section>
 </article>
--- src/stores.js
+++ src/stores.js
@@ -1,11 +1,77 @@
 import { writable } from 'svelte/store';
-import { get } from 'svelte/store';
 
-export const history = writable([]);
+// makes a record of a "New Circle" action for the history
+export function newCircleAction({ id, x, y, radius }) {
+    return {
+        undo: () => {
+            circles.update(c => {
+                delete c[id];
+                return c
+            });
+        },
+        redo: () => {
+            circles.update(c => {
+                c[id] = { id, x, y, radius };
+                return c
+            });
+        }
+    }
+}
 
-// custom store to implement undo / redo
-export const undo = () => { console.log(`Undo: ${get(history)}`) };
-export const redo = () => { console.log(`Redo: ${get(history)}`) };
+// makes a record of a "Changed Radius" action for the history
+export function changedRadiusAction({ id, prevRadius, newRadius }) {
+    return {
+        undo: () => {
+            circles.update(c => {
+                c[id].radius = prevRadius;
+                return c
+            });
+        },
+        redo: () => {
+            circles.update(c => {
+                c[id].radius = newRadius;
+                return c
+            });
+        }
+    }
+}
+
+// custom history store to implement undo / redo
+export function buildHistory() {
+    const { subscribe, set, update } = writable({
+        undo: [],
+        redo: [],
+    });
+
+    return {
+        subscribe,
+        do: action => update(({ undo, redo }) => {
+            // remember what we did so we can undo it later
+            undo.push(action);
+            // doing something new clears the redo buffer
+            redo = [];
+            return { undo, redo }
+        }),
+		undo: () => update(({ undo, redo }) => {
+            const action = undo.pop();
+            if (typeof action !== 'undefined') {
+                action.undo();
+                redo.push(action);
+            }
+            return { undo, redo }
+        }),
+		redo: () => update(({ undo, redo }) => {
+            const action = redo.pop();
+            if (typeof action !== 'undefined') {
+                action.redo();
+                undo.push(action);
+            }
+            return { undo, redo }
+        }),
+		clear: () => set({ undo: [], redo: [] }),
+	};
+}
 
 export const circles = writable({});
 export const selected = writable(null);
+export const history = buildHistory();
